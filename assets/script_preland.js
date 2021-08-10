78f4069f08207d9af1efd7ef5ab0745c23059c35
ibnuhamzah404 <ibnuhamzaah404@gmail.com>
2021-08-10 21:03:54 +0700

    new

--- assets/script_preland.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    window.addEventListener('load', function() {
-        for ( var i=0; i < document.links.length; i++ ){
-            document.links[i].setAttribute('href', landing_url);
-        }
-    });
-})();
